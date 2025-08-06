169734b0a008ba9039e670bce04f3422da64a931
shubhamchepe <34737632+shubhamchepe@users.noreply.github.com>
2025-08-06 12:44:36 +0530

    Update Message.js

--- models/Message.js
+++ models/Message.js
@@ -22,10 +22,21 @@ const Message = sequelize.define('Message', {
     type: DataTypes.STRING,
     allowNull: true, // Only needed for sent messages using templates
   },
-  content_variables: {
-    type: DataTypes.JSON,
-    allowNull: true, // Store key-value JSON if using templates
+ content_variables: {
+  type: DataTypes.TEXT,
+  allowNull: true,
+  get() {
+    const raw = this.getDataValue('content_variables');
+    try {
+      return JSON.parse(raw);
+    } catch {
+      return raw;
+    }
+  },
+  set(value) {
+    this.setDataValue('content_variables', JSON.stringify(value));
   }
+}
 }, {
   tableName: 'messages',
   timestamps: true, // Adds createdAt and updatedAt
